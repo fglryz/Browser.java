79e4d39a2c381ada2b0ddaa7c98f8a40979c4553
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-04 15:07:56 +0100

    day36_Inheritance

---
+++ src/day36_Inheritance/encapsulation/animalTask/Dog.java
@@ -0,0 +1,14 @@
+package day36_Inheritance.encapsulation.animalTask;
+
+public class Dog extends Animal {//dog is animal
+    // dog child class,animal parent class
+    public void bark(){
+        System.out.println(name+" is barking");
+    }
+
+}
+/*
+dog
+6 variables
+6 methods
+ */
