435c0eb851cc83ce5d16edeb577acbd5dc8ea614
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-04 15:15:42 +0100

    day36_Inheritance

---
+++ src/day36_Inheritance/encapsulation/animalTask/Cat.java
@@ -0,0 +1,19 @@
+package day36_Inheritance.encapsulation.animalTask;
+
+public class Cat extends Animal {//cat is an animal
+    public void scratch(){
+        System.out.println(name+" is scratching");
+
+    }
+    public void meow(){
+        System.out.println(name+" is meowing");
+
+    }
+
+
+}
+/*
+cat inherited
+6 variable
+6 methods +created 2 methods=8
+ */
---
+++ src/day36_Inheritance/encapsulation/animalTask/Tiger.java
@@ -0,0 +1,10 @@
+package day36_Inheritance.encapsulation.animalTask;
+
+public class Tiger extends  Animal{
+    public void roar(){
+        System.out.println(name+" is roaring");
+    }
+    public void hunt(){
+        System.out.println(name+" is hunting");
+    }
+}
