70a1ca59deb46547597f435c4af149c45a2c8a42
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-04 14:17:34 +0100

    day36_Inheritance

--- src/day36_interitance/StudentObject.java
+++ src/day36_interitance/StudentObject.java
@@ -2,10 +2,10 @@ package day36_interitance;
 
 public class StudentObject {
     public static void main(String[] args) {
-        Student student1=new Student("Daniel",
-                "MIT",2,'E','G');
-        student1.setAge(39);
-
+        day36_interitance.Student student1=new Student("Daniel","MIT",2,'G','A');
+        student1.setAge(34);
+        student1.setGender('M');
         System.out.println(student1);
+
     }
 }
