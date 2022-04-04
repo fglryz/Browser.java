1790fae67de6b950232697ba2fd2c09241c3e9f4
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-04 10:53:20 +0100

    encapsualtion

--- src/day35_Encapsulation/encapsulation/EmployeeObject.java
+++ src/day35_Encapsulation/encapsulation/EmployeeObject.java
@@ -2,7 +2,16 @@ package day35_Encapsulation.encapsulation;
 
 public class EmployeeObject {
     public static void main(String[] args) {
-        Employee e1=new Employee("Tatiana",'F',27,120000);
+        Employee e1=new Employee("Tatiana",'F',27,130000);
+        e1.setAge(32);
+
+        Employee e2=new Employee("Aygun",'F',24,110000);
+        e2.setName("Hulya");
+        e2.setSalary(e2.getSalary()+15000);
+
         System.out.println(e1);
+        System.out.println(e2);
+
+
     }
 }
