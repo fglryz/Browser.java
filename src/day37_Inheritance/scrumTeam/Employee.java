b39d3ef0fd0288c47750faf3ab03b6803a95b111
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-05 17:09:45 +0100

    day37_Inheritance

--- src/day37_Inheritance/scrumTeam/Employee.java
+++ src/day37_Inheritance/scrumTeam/Employee.java
@@ -1,7 +1,34 @@
 package day37_Inheritance.scrumTeam;
 
 public class Employee extends Person{
-    public Employee(String name, int age, char gender) {
+
+    public String jobTitle;
+    public int id;
+    public double salary;
+    public String companyName;
+
+    public Employee(String name, int age, char gender, String jobTitle, int id, double salary, String companyName) {
         super(name, age, gender);
+        this.jobTitle = jobTitle;
+        this.id = id;
+        this.salary = salary;
+        this.companyName = companyName;
+    }
+    public void work(){
+        System.out.println(jobTitle+" "+name+" "+ "is working");
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                "jobTitle='" + jobTitle + '\'' +
+                ", id=" + id +
+                ", salary=$" + salary +
+                ", companyName='" + companyName + '\'' +
+                '}';
     }
 }
+
