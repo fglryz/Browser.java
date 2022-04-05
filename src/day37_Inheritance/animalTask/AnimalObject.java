6ffc69b7b84d2a3bec0a0dbb04537e59bd6300ed
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-05 13:42:52 +0100

    day36_Inheritance

---
+++ src/day37_Inheritance/animalTask/AnimalObject.java
@@ -0,0 +1,29 @@
+package day37_Inheritance.animalTask;
+
+public class AnimalObject {
+    public static void main(String[] args) {
+
+
+        Cat cat1 = new Cat("Leo", "Short Hair", 'M',
+                "medium", "Golden Brown", 1);
+        cat1.drink();
+        cat1.scratch();
+        cat1.move();
+
+
+        System.out.println(cat1);
+
+        Dog dog1 = new Dog("Max", "Hasky", 'F', "Big",
+                "White", 2);
+        System.out.println(dog1);
+        dog1.bark();
+        dog1.sleep();
+
+        Parrot parrot1=new Parrot("King","Makaw",'M',"Small","Blue",2,
+                "Yellow");
+        System.out.println(parrot1);
+        parrot1.sing();
+
+
+    }
+}
--- src/day37_Inheritance/animalTask/Parrot.java
+++ src/day37_Inheritance/animalTask/Parrot.java
@@ -1,12 +1,15 @@
 package day37_Inheritance.animalTask;
 
 public class Parrot extends Animal {
-    public int a;
+    //public String parrotWingColor;
 
-    public Parrot(String name, String breed, char gender, String size, String color, int age) {
+
+    public Parrot(String name, String breed, char gender, String size, String color, int age,String parrotWingColor) {
         super(name, breed, gender, size, color, age );
+       // this.parrotWingColor=parrotWingColor;
     }
     public void sing(){
         System.out.println(name+" is singing");
     }
+
 }
