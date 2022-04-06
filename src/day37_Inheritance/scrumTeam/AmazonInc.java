4aff27877989905bfee842ed596ffbb16ef85467
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-06 11:21:23 +0100

    day37-inheritance

--- src/day37_Inheritance/scrumTeam/AmazonInc.java
+++ src/day37_Inheritance/scrumTeam/AmazonInc.java
@@ -4,7 +4,7 @@ public class AmazonInc {
     public static void main(String[] args) {
         String company="Amazon Inc";
 
-        ProoductOwner po=new ProoductOwner("Shuaip",29,'M',1,160000,company);
+        ProoductOwner po=new ProoductOwner("Shuap",29,'M',1,160000,company);
         BusinessAnalyst ba = new BusinessAnalyst("Cihad", 30, 'M', 2, 150000,company);
         ScrumMaster sm = new ScrumMaster("Anel", 26, 'F', 3, 145000, company);
 
