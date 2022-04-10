e0497711122572937736b4192b8a6bc1250ba891
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-10 14:06:03 +0100

    day39-Recap

--- src/day39_Recap/shape/Circle.java
+++ src/day39_Recap/shape/Circle.java
@@ -1,6 +1,6 @@
 package day39_Recap.shape;
 
-public class Circle {
+public class Circle extends Shape{
 
     private double radius;
     public static double pi=3.14;
@@ -11,10 +11,33 @@ public class Circle {
 
     public void setRadius(double radius) {
         if(radius<=0){
-            System.out.println("Invalid radius"+radius);
+            System.err.println("Invalid radius"+radius);
             System.exit(1);
         }
         this.radius = radius;
+    }
+
+    public Circle( double radius) {
+        super("Circle");
+        setRadius(radius);
+    }
+
+    @Override
+    public double area() {
+        return radius * radius * pi;
+    }
+
+    @Override
+    public double perimeter() {
+      return  radius * radius * pi;
+    }
+
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
     }
 }
 /*
@@ -25,6 +48,6 @@ pi (static)
 Encapsulate the field
 Add a constructor to set the filed
 area(): radius * radius * pi
-perimeter(): 2 * radius * pi
+perimeter(): radius * radius * pi
 toString(): r, pi, area, perimeter
  */
