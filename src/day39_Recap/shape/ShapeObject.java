7a65e4f52a078e9cdb532e3ecf665d3d55dbb8e9
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-10 14:45:26 +0100

    day39-Recap

--- src/day39_Recap/shape/ShapeObject.java
+++ src/day39_Recap/shape/ShapeObject.java
@@ -4,24 +4,30 @@ public class ShapeObject {
     public static void main(String[] args) {
         Rectangle rectangle=new Rectangle(3,2);
         System.out.println(rectangle);
+        rectangle.setLength(10);
+        System.out.println(rectangle.getLength());
+        System.out.println(rectangle);
+        System.out.println(rectangle.getName());
 
-        System.out.println("area=" + rectangle.area());
-        System.out.println( "Rectangle "+rectangle.perimeter());
 
         System.out.println("==============================");
 
        Square square=new Square(5);
         System.out.println(square);
         square.setSide(15);
-        System.out.println("area=" + square.area());
-        System.out.println( "Rectangle "+square.perimeter());
+
 
         System.out.println("==============================");
 
         Circle circle=new Circle(4);
         System.out.println(circle);
-        System.out.println("area=" + circle.area());
-        System.out.println( "Rectangle "+circle.perimeter());
+        circle.setRadius(9.5);
+        System.out.println(circle.getRadius());
+        System.out.println(circle);
+        System.out.println(circle.getName());
+
+
+
 
 
     }
