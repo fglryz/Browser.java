1b69223afb962fa7125972469ee2c74fa02b8338
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-10 14:31:15 +0100

    day39-Recap

--- src/day39_Recap/shape/ShapeObject.java
+++ src/day39_Recap/shape/ShapeObject.java
@@ -2,6 +2,27 @@ package day39_Recap.shape;
 
 public class ShapeObject {
     public static void main(String[] args) {
+        Rectangle rectangle=new Rectangle(3,2);
+        System.out.println(rectangle);
+
+        System.out.println("area=" + rectangle.area());
+        System.out.println( "Rectangle "+rectangle.perimeter());
+
+        System.out.println("==============================");
+
+       Square square=new Square(5);
+        System.out.println(square);
+        square.setSide(15);
+        System.out.println("area=" + square.area());
+        System.out.println( "Rectangle "+square.perimeter());
+
+        System.out.println("==============================");
+
+        Circle circle=new Circle(4);
+        System.out.println(circle);
+        System.out.println("area=" + circle.area());
+        System.out.println( "Rectangle "+circle.perimeter());
+
 
     }
 }
