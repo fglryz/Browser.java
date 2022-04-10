34ba3ab42680ab6a8d02849b9c0d98d7d21a0895
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-10 16:06:38 +0100

    day39-Recap

---
+++ src/day39_Recap/cydeoTask/Developer.java
@@ -0,0 +1,20 @@
+package day39_Recap.cydeoTask;
+
+public class Developer extends Employee {
+    public Developer(String name, int age, char gender, String employeeId, String jobTitle, double salary) {
+        super(name, age, gender, employeeId, jobTitle, salary);
+    }
+    public void fixingBugs(){
+        System.out.println(getJobTitle()+" "+getName()+" is fixing Bug");
+    }
+    @Override
+    public void work() {
+        System.out.println(getJobTitle()+" "+getName()+" is working");
+    }
+}
+/*
+4. Create a sub class of Employee named Developer
+Override the work method
+Extra methods:
+fixingBugs()
+ */
