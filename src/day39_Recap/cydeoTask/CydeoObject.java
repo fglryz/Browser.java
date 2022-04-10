3b63686618244569ce7800efcfbce52b18c811f2
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-10 16:42:33 +0100

    day39-Recap

---
+++ src/day39_Recap/cydeoTask/CydeoObject.java
@@ -0,0 +1,14 @@
+package day39_Recap.cydeoTask;
+
+public class CydeoObject {
+    public static void main(String[] args) {
+        Tester tester=new Tester("Alina",34,'F',"123456","SDET",100000);
+
+    }
+}
+/*
+Create a class named Cydeo:
+create objects of Tester, Developer,Teacher, Student
+test all the functions of each objects
+Analyze the relationships between the classes
+ */
