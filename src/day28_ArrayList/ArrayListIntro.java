fa5186d16b3fba1c0f7efe73fd9014c9f063d3a3
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-12 11:04:34 +0100

    day28-ArrayList

--- src/day28_ArrayList/ArrayListIntro.java
+++ src/day28_ArrayList/ArrayListIntro.java
@@ -8,9 +8,14 @@ public class ArrayListIntro {
 
 
         ArrayList<Integer>numbers=new ArrayList<>();
-        ArrayList<String>names=new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
         System.out.println(numbers);
-        System.out.println(names);
+
+
+
+
 
 
     }
