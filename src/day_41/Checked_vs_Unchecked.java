0c0cb12f3bc96411329dbf122ad37d49ba7047f6
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-14 15:09:59 +0100

    Day 41-Exception

---
+++ src/day_41/Checked_vs_Unchecked.java
@@ -0,0 +1,35 @@
+package day_41;
+
+import day39_Recap.cydeoTask.Student;
+
+import java.io.FileInputStream;
+
+public class Checked_vs_Unchecked {
+    public static void main(String[] args) {
+        //unchecked exception
+        int a=10;
+        int b=0;
+       // System.out.println(a/b);
+        //System.out.println("WoodenSpoon");
+        char[] characters={'A','B','C'};
+                          // 0  1    2
+        //System.out.println(characters[99]);
+        Student student=null;
+       // student.study();
+        //System.out.println(student.getName());
+        String str="Wooden Spoon";//unchecked
+       //str=null;
+       //System.out.println(str.toUpperCase());
+        System.out.println("------------------------------");
+        //checked Exception:
+
+        System.out.println("Hello");
+        //Thread.sleep(3000);//checked
+
+        System.out.println("Cydeo");
+
+        //FileInputStream file=new FileInputStream("path of the file");
+
+    }
+
+}
