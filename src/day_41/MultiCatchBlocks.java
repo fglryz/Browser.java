104c0ea3df12f5193480a592ef8137df145c2daa
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-15 10:37:57 +0100

    day 41

---
+++ src/day_41/MultiCatchBlocks.java
@@ -0,0 +1,36 @@
+package day_41;
+
+import day39_Recap.cydeoTask.Employee;
+
+public class MultiCatchBlocks {
+    public static void main(String[] args) {
+        Employee employee=null;
+        try {
+            System.out.println(employee.getSalary());
+        }catch(NullPointerException e){
+            System.out.println("zero catch block");
+            e.printStackTrace();
+
+        }
+        catch (IndexOutOfBoundsException e){
+            System.out.println("first catch block");
+            e.printStackTrace();
+
+        } catch (ArithmeticException e){
+            System.out.println("second catch block");
+            e.printStackTrace();
+
+        } catch (ClassCastException e){
+            System.out.println("third catch block");
+
+            e.printStackTrace();
+        } catch (RuntimeException e){
+            System.out.println("fourth catch block");
+            e.printStackTrace();
+        }
+
+
+        System.out.println("test completed");
+
+    }
+}
