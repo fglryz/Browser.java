6b44d1fe5c8ac68a84934e5281fc596643279177
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-18 10:38:10 +0100

    day 42 Exception

---
+++ src/day_42_Exceptions/ThrowsKeywords2.java
@@ -0,0 +1,11 @@
+package day_42_Exceptions;
+
+public class ThrowsKeywords2 {
+    public static void main(String[] args) throws ArithmeticException{
+
+       // System.out.println("Test started");
+       // System.out.println(8/0);
+       // System.out.println("Test completed");
+    }
+
+}
