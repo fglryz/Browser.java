71819d8696e4becb6146562d59f8a62f44b8473d
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-18 10:21:16 +0100

    day 42 Exception

---
+++ src/day_42_Exceptions/DisadvantageOfThrowsKeyword.java
@@ -0,0 +1,30 @@
+package day_42_Exceptions;
+
+import utilities.Sleep;
+
+import javax.swing.text.Utilities;
+
+public class DisadvantageOfThrowsKeyword {
+
+
+    public static void main(String[] args)  {
+        System.out.println("Hello");
+       // sleep(2.5);//throws
+        System.out.println("Hello World");
+        System.out.println("-----------------------");
+        System.out.println("Hello");
+        MorningWorkout.sleep(2.5);
+        System.out.println("Cydeo");
+
+    }
+    public static void sleep(double second) throws InterruptedException {
+        Thread.sleep((long) (second*1000));
+    }
+
+
+
+
+    }
+
+
+
