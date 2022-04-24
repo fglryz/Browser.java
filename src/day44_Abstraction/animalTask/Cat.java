d59d8b1d203df11e38135f665cfddbd983b1fcda
Fatma Guleryuz <fglryz.111@gmail.com>
2022-04-24 15:10:48 +0100

    day44 Abstraction

--- src/day44_Abstraction/animalTask/Cat.java
+++ src/day44_Abstraction/animalTask/Cat.java
@@ -1,4 +1,19 @@
 package day44_Abstraction.animalTask;
 
-public class Cat {
+public final class Cat extends Animal{
+
+    public Cat(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    public void meow(){
+        System.out.println(getName()+"is  meowing");
+
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName()+" is eating biryani");
+
+    }
 }
--- src/day44_Abstraction/animalTask/Dog.java
+++ src/day44_Abstraction/animalTask/Dog.java
@@ -1,5 +1,19 @@
 package day44_Abstraction.animalTask;
 
-public class Dog {
+public final class Dog extends Animal{
 
+    public Dog(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    public void bark(){
+        System.out.println(getName()+" is barking");
+
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName()+" eats Pizza");
+
+    }
 }
