c8187891d0c0c955e9f60764d4924e77a2878741
Fatma Guleryuz <fglryz.111@gmail.com>
2022-05-04 09:37:49 +0100

    day45 Abstraction

---
+++ src/day45_Abstraction/PropertiesOfInterface.java
@@ -0,0 +1,23 @@
+package day45_Abstraction;
+
+public interface PropertiesOfInterface {
+    int a=100;
+    static int b=50;
+    /*
+    public PropertiesOfInterface(int a){
+        this.a=a;
+    }*//*
+    static {
+        b=100;
+    }*/
+    //public void method1(){
+       // System.out.println("Instance method);}
+
+    public static void method2(){
+        System.out.println("Static method");
+    }
+     void method3();
+    default void method4(){
+        System.out.println("Default method ");
+    }
+}
